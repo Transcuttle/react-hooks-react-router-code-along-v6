759daaaad8c2cec8f32be7acdf4943d4b24eb437
Sasha <georgefbrockman@gmail.com>
2024-01-17 13:01:15 -0600

    completes code along to dynamic routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserProfile from "./pages/UserProfile";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/profile",
+        element: <UserProfile />
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
